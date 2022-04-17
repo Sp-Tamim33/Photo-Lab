b38f01ce4c84536c6410483149766bbf90bc6aff
sp-tam1m <white.hat.tamim@gmail.com>
2022-04-17 14:42:58 +0600

    router added and router set up added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Components/About/About';
+import Blog from './Components/Blog/Blog';
 import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Login from './Components/Login/Login';
+import NotFound from './Components/NotFound/NotFound';
+import Signup from './Components/Signup/Signup';
+
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>} />
+        <Route path='/home' element={<Home></Home>} />
+        <Route path='/about' element={<About></About>} />
+        <Route path='/blog' element={<Blog></Blog>} />
+        <Route path='/login' element={<Login></Login>} />
+        <Route path='/signup' element={<Signup></Signup>} />
+        <Route path='*' element={<NotFound></NotFound>} />
+      </Routes>
     </div>
   );
 }
