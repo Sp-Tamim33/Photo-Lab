2c872e86c7a4b72a875cc2c74595427f476457c1
sp-tam1m <white.hat.tamim@gmail.com>
2022-04-17 15:56:41 +0600

    home page banner added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Components/About/About';
 import Blog from './Components/Blog/Blog';
+
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Login from './Components/Login/Login';
